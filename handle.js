0a77d579f8f877e289562f6b45ae24aa6f37ba54
Nikita Lvov <lvov.nikita@yahoo.com>
2019-11-08 16:59:54 +0300

    27 задач в JSON

--- handle.js
+++ handle.js
@@ -115,84 +115,84 @@ $(() => {
         "title": "Чётное вхождение",
         "text": "Дан массив. Только одно число в этом массиве встречается чётное кол-во раз. Найдите и выведите это число.",
         "example": "",
-        "js_answer": "a = [1,2,3,4,5,6,2,7,8,9]; obj = {}; a.forEach((elem) => { obj[elem] == undefined ? obj[elem] = 1 : obj[elem] += 1; }); for(elem in obj) { if(obj[elem] % 2 == 0) {console.log(elem)};"
+        "js_answer": "a = [1,2,3,4,5,6,2,7,8,9];\nobj = {};\na.forEach((elem) => {\n\tobj[elem] == undefined ? obj[elem] = 1 : obj[elem] += 1;\n});\nfor(elem in obj) {\n\tif(obj[elem] % 2 == 0) {console.log(elem)};\n};"
       },
       {
         "id": 17,
         "title": "Массив квадратов",
         "text": "Сравните два массива. Элементы массива b должны быть квадратами элементов массива a. Элементы в массивах могут быть расположены в любом порядке.",
         "example": "a = [121, 144, 19, 161, 19, 144, 19, 11] <br>b = [121, 14641, 20736, 361, 25921, 361, 20736, 361] <br><br></code>Напишите функцию comp(a, b), которая вернёт true, если: <br><br><code>a = [121, 144, 19, 161, 19, 144, 19, 11] <br>b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19] <br><br></code>или вернётся false, если элементы одного массива не являются квадратами другого: <br><br><code>a = [121, 144, 19, 161, 19, 144, 19, 11] <br>b = [132, 14641, 20736, 361, 25921, 361, 20736, 361] <br><br></code> (так как 132 не является квадратом ни одного элемента из массива a) <br>",
-        "js_answer": "a = [121, 144, 19, 161, 19, 144, 19, 11]; b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]; comp = (a, b) => { a = a.sort((x, y) => (x - y)); b = b.sort((x, y) => (x - y)); for (i = 0; i < a.length; i++) { if (Math.pow(a[i], 2) != b[i]) { return false } } return true; } console.log(comp(a,b));"
+        "js_answer": "a = [121, 144, 19, 161, 19, 144, 19, 11];\nb = [121, 14641, 20736, 361, 25921, 361, 20736, 361];\ncomp = (a, b) => {\n\ta = a.sort((x, y) => (x - y));\n\tb = b.sort((x, y) => (x - y));\n\tfor (i = 0; i < a.length; i++){\n\t\tif (Math.pow(a[i], 2) != b[i]) {\n\t\t\treturn false\n\t\t}\n\t}\n\treturn true;\n}\nconsole.log(comp(a,b));"
       },
       {
         "id": 18,
         "title": "DJ Поликарп",
         "text": "Поликарп работает DJ в ночном клубе и часто использует dubstep в своих выступлениях. Недавно он решил взять несколько известных песен и сделать из них ремиксы. Чтобы сделать ремикс песни, Поликарп вставляет несколько слов (может быть ноль) «WUB» перед первым словом в песне, поcле последнего слова (может быть ноль), и между словами (как минимум одно между каждой парой слов). Затем Поликарп склеивает все слова, включая «WUB», в одну строку и играет песню в клубе. Например, песня со словами «I AM X» может стать ремиксом: <br><br> 'WUBWUB<b>I</b>WUB<b>AM</b>WUBWUB<b>X</b>' <br><br> и не может быть: <br><br> 'WUBWUB<b>IAM</b>WUB<b>X</b>' (нет WUB между I и AM) <br><br> Недавно Джонни слушал новые треки Поликарпа, но так как он не слишком любит современную музыку, ему стало интересно, как звучали оригинальные треки. Помогите Джонни восстановить оригинальные песни.",
         "example": "song_decoder('WUB<u>WE</u>WUB<u>ARE</u>WUBWUB<u>THE</u>WUB<u>CHAMPIONS</u>WUB<u>MY</u>WUBWUBWUB<u>FRIEND</u>WUB') == 'WE ARE THE CHAMPIONS MY FRIEND'<br>",
-        "js_answer": "song_decoder = a => { a = a.split('WUB').filter(elem => {return elem != ''}).join(' '); console.log(a);} song_decoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBWUBWUBFRIENDWUB');"
+        "js_answer": "song_decoder = a => {\na = a.split('WUB').filter(elem => {return elem != ''}).join(' ');\nconsole.log(a);} song_decoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBWUBWUBFRIENDWUB');"
       },
       {
         "id": 19,
         "title": "Самое короткое слово",
         "text": "Вычислите длину самого короткого слова в строке.",
         "example": "",
-        "js_answer": "a = 'WE ARE THE CHAMPIONS MY FRIEND'; a = a.split(' ').sort((x, y) => x.length - y. length)[0].length; console.log(a);"
+        "js_answer": "a = 'WE ARE THE CHAMPIONS MY FRIEND';\na = a.split(' ').sort((x, y) => x.length - y. length)[0].length;\nconsole.log(a);"
       },
       {
         "id": 20,
         "title": "Двоичное сложение",
         "text": "Напишите функцию, которая складывает два двоичных числа и возвращает результат в двоичном виде.",
         "example": "",
-        "js_answer": "a = 0b10101, b = 0b1010; console.log((a + b).toString(2));"
+        "js_answer": "a = 0b10101, b = 0b1010;\nconsole.log((a + b).toString(2));"
       },
       {
         "id": 21,
         "title": "Нечётная пирамида",
         "text": "Дан треугольник нечётных чисел: <br><br>1 <br>3 5 <br>7 9 11 <br>13 15 17 19 <br>21 23 25 27 29 <br>... <br><br> Напишите функцию, которая считает сумму элементов в строке по номеру строки:",
         "example": "row_sum_odd_numbers(1) == 1 <br> row_sum_odd_numbers(2) == 8 <br>",
-        "js_answer": "row_sum_odd_numbers = a => {first = 0, result = 0; for (i = 0; i < a; i++) {first += i;} first = first * 2 + 1; for (i = first ; i <= first + 2 * (a - 1); i += 2) {result += i;} console.log(result); } row_sum_odd_numbers(4);"
+        "js_answer": "row_sum_odd_numbers = a => {\n\tfirst = 0, result = 0;\n\tfor (i = 0; i < a; i++) {first += i;}\n\tfirst = first * 2 + 1;\n\tfor (i = first ; i <= first + 2 * (a - 1); i += 2) {result += i;}\n\tconsole.log(result);\n}\nrow_sum_odd_numbers(4);"
       },
       {
         "id": 22,
         "title": "Следующий квадрат",
         "text": "Напишите функцию findNextSquare, которая в качестве аргумента принимает квадрат какого-то числа и возвращает квадрат следующего числа. Если аргумент не является квадратом, вернуть -1.",
         "example": "findNextSquare(121) == 144 <br> findNextSquare(625) == 676 <br> findNextSquare(114) == -1 // так как 114 не является квадратом какого-либо числа<br>",
-        "js_answer": "findNextSquare = a => {return(Math.pow(Math.sqrt(a) + 1, 2))} console.log(findNextSquare(121));"
+        "js_answer": "findNextSquare = a => {return(Math.pow(Math.sqrt(a) + 1, 2))}\nconsole.log(findNextSquare(121));"
       },
       {
         "id": 23,
         "title": "Очередь в супермаркете",
         "text": "В супермаркете большая очередь на кассах. Вам нужно написать функцию, которая рассчитает время, необходимое покупателями для оплаты покупок. <br><br>В функцию поступает две переменные: <ul> <li>customers — массив положительных целых чисел, представляющий очередь. Каждый элемент массива означает сколько времени необходимо покупателю для оплаты товара; <li>N — число касс </ul> Функция должна вернуть, сколько времени потребуется всем покупателям для оплаты товара. <br><br> Существует всего одна очередь и несколько касс. Покупатели идут в порядке очереди. Как только одна из касс освобождается, туда идёт следующий покупатель из очереди. <br><br>",
         "example": "queue_time([5,3,4], 1) == 12 <br>// так как у нас одна касса, 5 + 4 + 3 = 12 <br><br> queue_time([10,2,3,3], 2) == 10 <br> // так как у нас две кассы и пока первый покупатель будет стоять в первой кассе, второй, третий и четвёртый уже оплатят товар (2 + 3 + 3) <br><br> queue_time([2,3,10], 2) == 12 <br> // первый покупатель идёт в первую кассу, второй – во вторую. Первая касса освобождается быстрее второй и туда идёт третий покупатель. Поэтому на первой кассе время обслуживания = 2 + 10 = 12, а на второй = 3 <br>",
-        "js_answer": "queue_time = (a, n) => { cashboxes = new Array(n).fill(0); for (elem in a) {cashboxes[cashboxes.indexOf(Math.min(...cashboxes))] += a[elem]}; return (Math.max(...cashboxes)); } console.log(queue_time([10,2,3,3], 2));"
+        "js_answer": "queue_time = (a, n) => {\n\tcashboxes = new Array(n).fill(0);\n\tfor (elem in a) {cashboxes[cashboxes.indexOf(Math.min(...cashboxes))] += a[elem]};\n\treturn (Math.max(...cashboxes));\n}\nconsole.log(queue_time([10,2,3,3], 2));"
       },
       {
         "id": 24,
         "title": "Изограммы",
         "text": "Изограмма — слово, в котором нет повторяющихся букв. Напишите функцию, которая проверит, является слово изограммой или нет.",
         "example": "is_isogram('Dermatoglyphics') == true <br>is_isogram('aba') == false <br>is_isogram('moOse') == false <br>",
-        "js_answer": "is_isogram = a => {a = a.toLowerCase();console.log(a);obj = {};for (elem in a) {if (obj[a[elem]] == undefined) {obj[a[elem]] = 1;} else {return false;}}return true;}console.log(is_isogram('Dermatoglyphics'));"
+        "js_answer": "is_isogram = a => {\n\ta = a.toLowerCase();\n\tconsole.log(a);\n\tobj = {};\n\tfor (elem in a) {\n\t\tif (obj[a[elem]] == undefined) {\n\t\t\tobj[a[elem]] = 1;\n\t\t} else {\n\t\t\treturn false;\n\t\t}\n\t}\n\treturn true;\n}\nconsole.log(is_isogram('Dermatoglyphics'));"
       },
       {
         "id": 25,
         "title": "Ошибки печати",
         "text": "На заводе принтер печатает наклейки для ящиков. Для каждого типа коробок принтер должен использовать цвет, которой для простоты обозначается буквами от a до m. Цвета, используемые принтером записываются в контрольную строку. Например, корректная контрольная строка будет выглядеть как aaabbbbhaijjjm, что значит, что принтер три раза использовал букву a, четыре раза букву b и так далее. Когда возникают проблемы, контрольная строка получается некорректной, например: aaa<b>x</b>bbbb<b>yy</b>h<b>w</b>a<b>w</b>i<b>w</b>jjj<b>ww</b>m <br><br>Вам нужно написать функцию printer_error, которая по входной строке выдаст частоту ошибок принтера, которая вычисляется как отношение количества ошибок к длине контрольной строки. Не сокращайте данную строку! <br><br>Контрольная строка может состоять только из букв от a до z.",
         "example": "error_printer('aaabbbbhaijjjm') == 0/14 <br>error_printer('aaaxbbbbyyhwawiwjjjwwm') == 8/22 <br>",
-        "js_answer": "error_printer = a => {errors = 0;for (elem in a) {a[elem] > 'm' ? errors += 1 : 0};console.log(errors);}error_printer('aaaxbbbbyyhwawiwjjjwwm');"
+        "js_answer": "error_printer = a => {\n\terrors = 0;\n\tfor (elem in a) {a[elem] > 'm' ? errors += 1 : 0};\n\tconsole.log(errors);\n}\nerror_printer('aaaxbbbbyyhwawiwjjjwwm');"
       },
       {
         "id": 26,
         "title": "Является ли треугольником?",
         "text": "Напишите функцию, которая принимает три целых числа a, b, c. Функция должна вернуть true, если может быть построен треугольник со сторонами a, b, c и false в противном случае.",
         "example": "",
-        "js_answer": "is_triangle = (a, b, c) => {sides = [a, b, c].sort((x, y) => {return x - y});if (sides[0] + sides[1] > sides[2]) {return true};return false;}console.log(is_triangle(11, 6, 6));"
+        "js_answer": "is_triangle = (a, b, c) => {\n\tsides = [a, b, c].sort((x, y) => {return x - y});\n\tif (sides[0] + sides[1] > sides[2]) {return true};\n\treturn false;\n}\nconsole.log(is_triangle(11, 6, 6));"
       },
       {
         "id": 27,
         "title": "Подпрыгивающие мячи",
         "text": "Ребёнок играет с мячиком на n-ом этаже высотного здания. Высота этого этажа h известна. Он бросает мячик из окна. Мяч подпрыгивает, например, на две-трети высоты h (bounce = 0.66). Его мама смотрит в окно на высоте 1.5 метра от земли. Как много раз она увидит пролетающий мимо её окна мяч (считая, когда он пролетает и вниз и вверх)? <br><br>Три условия должны соблюдаться для чистоты эксперимента:<ul><li>h должна быть больше 0</li><li>bounce больше 0, но меньше 1</li><li>уровень окна мамы меньше, чем h</li></ul>Если все три условия соблюдаются, верните результат в виде целого числа, если нет — верните –1.",
         "example": "h = 3, bounce = 0.66, window = 1.5, результат = 3 <br>h = 3, bounce = 1, window = 1.5, результат = -1 <br>",
-        "js_answer": "bounce = (h, bounce, wndw) => {if (h > 0 && 1 > bounce > 0 && wndw < h) {res = 0;for (; h >= wndw; h *= bounce) {res += 1};return res*2 - 1;}return -1;}console.log(bounce(3, 0.66, 1.5));"
+        "js_answer": "bounce = (h, bounce, wndw) => {\n\tif (h > 0 && 1 > bounce > 0 && wndw < h) {\n\t\tres = 0;\n\t\tfor (; h >= wndw; h *= bounce) {res += 1};\n\t\treturn res*2 - 1;\n\t}\n\treturn -1;\n}\nconsole.log(bounce(3, 0.66, 1.5));"
       },
       {
         "id": 28,
